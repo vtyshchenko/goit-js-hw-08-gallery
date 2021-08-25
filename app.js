e5d0f2faa3aa1c092243c120a0d81c4baba1e85d
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-08-25 18:22:59 +0300

    all works

--- app.js
+++ app.js
@@ -63,27 +63,48 @@ const galleryItems = [
     description: "Lighthouse Coast Sea",
   },
 ];
+
 const ulListRef = document.querySelector(".js-gallery");
+const modelWindowRef = document.querySelector(".js-lightbox");
+const menuBtnRef = document.querySelector(".lightbox__button");
+const imageRef = document.querySelector(".lightbox__image");
+
+// const imagesRef = document.querySelector(".lightbox__content");
 ulListRef.classList.add("list");
 
 // Создание и рендер разметки по массиву данных galleryItems из app.js
 // и предоставленному шаблону.
 let s = ``;
 for (const image of galleryItems) {
-  s += `<li class="gallery__item"><a class="gallery__link" href="${image["original"]}" > <img class="gallery__image" src="${image["preview"]}" data-source="${image["original"]}" alt="${image["description"]}"/></a></li>`;
+  // s += `<li class="gallery__item"><a class="gallery__link" href="${image["original"]}" > <img class="gallery__image" src="${image["preview"]}" data-source="${image["original"]}" alt="${image["description"]}"/></a></li>`;
+  s += `<li class="gallery__item"><img class="gallery__image" src="${image["preview"]}" data-source="${image["original"]}" alt="${image["description"]}"/></li>`;
 }
 ulListRef.insertAdjacentHTML("beforeend", s);
 
 // Реализация делегирования на галерее ul.js-gallery и получение url
 // большого изображения.
 
-// Открытие модального окна по клику на элементе галереи.
+ulListRef.addEventListener("click", onClickPicture);
+menuBtnRef.addEventListener("click", onClickButtonClose);
 
-// Подмена значения атрибута src элемента img.lightbox__image.
+function onClickPicture(params) {
+  // Открытие модального окна по клику на элементе галереи.
+  // Подмена значения атрибута src элемента img.lightbox__image.
+  imageRef.setAttribute("src", params.target.attributes["data-source"].value);
+  imageRef.setAttribute("alt", params.target.alt);
 
-// Закрытие модального окна по клику на кнопку
-// button[data - action= "close-lightbox"].
+  modelWindowRef.classList.add("is-open");
+}
 
-// Очистка значения атрибута src элемента img.lightbox__image.
-// Это необходимо для того, чтобы при следующем открытии модального окна,
-// пока грузится изображение, мы не видели предыдущее.
+function onClickButtonClose(params) {
+  if (menuBtnRef.dataset.action === "close-lightbox") {
+    // Закрытие модального окна по клику на кнопку
+    // button[data-action= "close-lightbox"].
+    modelWindowRef.classList.toggle("is-open");
+    // Очистка значения атрибута src элемента img.lightbox__image.
+    // Это необходимо для того, чтобы при следующем открытии модального окна,
+    // пока грузится изображение, мы не видели предыдущее.
+    imageRef.setAttribute("src", "");
+    imageRef.setAttribute("alt", "");
+  }
+}
